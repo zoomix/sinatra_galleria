1e234c789da4e695c47d83fdaa9fc348a9f1de1f
Srdan Kvrgic <srdan@burtcorp.com>
2012-08-19 14:00:06 +0200

    Being more memory efficient.

--- Rakefile
+++ Rakefile
@@ -7,7 +7,6 @@ desc "Generats thumbnails"
 task :thumb_gen do
   Albums.list.each do |album|
     album.images.each do |image|
-      puts "Generating thumb for #{image.name} in #{album.name}. #{image.thumb_path}"
       ThumbGenerator.generate_thumb(image) unless image.thumb
     end
   end
--- lib/thumb_generator.rb
+++ lib/thumb_generator.rb
@@ -7,9 +7,18 @@ module ThumbGenerator
     dir_path = File.dirname(original_image.thumb_path)
     FileUtils.mkdir_p(dir_path) unless Dir.exists?(dir_path)
 
-    img = Magick::Image.read(original_image.path).first
-    thumb = img.resize_to_fill(160, 160)
-    thumb.write(original_image.thumb_path) {self.quality = 70}
+    begin
+      img = Magick::Image.read(original_image.path).first
+      thumb = img.resize_to_fill(160, 160)
+      thumb.write(original_image.thumb_path) {self.quality = 70}
+
+      img.destroy!
+      thumb.destroy!
+
+      puts "generated #{original_image.thumb_path}"
+    rescue Exception => e
+      puts "skipping #{original_image.path}"
+    end
   end
 
 end

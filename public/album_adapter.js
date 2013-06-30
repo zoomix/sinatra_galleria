b26719f559601f35916cff8619fb2cc7f36125eb
Srdan Kvrgic <srdan@burtcorp.com>
2013-06-30 21:21:59 +0200

    Lazyloading images instead of pushing them in/out

--- public/album_adapter.js
+++ public/album_adapter.js
@@ -8,30 +8,30 @@ var Album = function(albumName, thumbSize, urls) {
   self.expanded = false;
 
   self.renderAlbum = function() {
-    var nofImagesOnFirstLine = Math.floor($(window).width() / thumbSize);
-
-    var htmlz = self.getAlbumHtml(nofImagesOnFirstLine, self.expanded);
-
-    $('#' + albumName + '_container').html(htmlz);
-    $('#' + albumName + '_container').css('height', self.expanded? 'auto' : '');
-    //$('\.' + albumName).touchGallery();
+    var htmlz = self.getAlbumHtml(self.expanded);
+    album.inner_cont().html(htmlz);
+    album.inner_cont().css('width', (album.urls.length * album.thumbSize)+'px' );
     $('\.' + albumName).photoSwipe();
-
+    $('\.' + albumName + ' img').lazyload({container: self.outer_cont() });
   };
 
-  self.getAlbumHtml = function(nofImagesOnFirstLine, fullyVisible) {
+  self.getAlbumHtml = function(fullyVisible) {
     var htmlz = '';
     for(var i = 0; i < urls.length; i++) {
       if(urls[i]) {
         htmlz += "<a class='" + albumName + "' href='" + urls[i]['image'] + "'>";
-        if(fullyVisible || i < nofImagesOnFirstLine) {
-          htmlz += "<img src='" + urls[i]['thumb'] + "' alt='" + albumName + "'>";
-        }
+        htmlz += "<img width='" + self.thumbSize + "' height='" + self.thumbSize + "' src='css/images/overlay.png' data-original='" + urls[i]['thumb'] + "' alt='" + albumName + "'>";
         htmlz += "</a>";
       }
     }
-    // htmlz += "<br style='clear:both' /><div class='toggler' onClick='javascript:toggle(\"" + albumName + "\")'>toggle</div>"
     return htmlz;
+  };
+
+  self.outer_cont = function() {
+    return $('#' + self.albumName + '_container');
+  };
+  self.inner_cont = function() {
+    return $('#' + self.albumName + '_inner');
   };
 
   return this;
@@ -40,15 +40,19 @@ var Album = function(albumName, thumbSize, urls) {
 var toggle = function(albumName) {
   var album = albums[albumName];
   album.expanded = !album.expanded;
-  album.renderAlbum();
+  if(album.expanded) {
+    album.outer_cont().css('overflow', 'scroll');
+  } else {
+    album.outer_cont().css('overflow', 'hidden');
+  }
 }
 
-var albumResizer = function(albumResizerSeed) {
-  $.each(albums, function(album_name) { 
-    album = albums[album_name];
-    if(!album.expanded) {
-      album.renderAlbum()
-    }
-  } );
-}
-window.onresize = albumResizer
+// var albumResizer = function(albumResizerSeed) {
+//   $.each(albums, function(album_name) { 
+//     album = albums[album_name];
+//     if(!album.expanded) {
+//       album.renderAlbum()
+//     }
+//   } );
+// }
+// window.onresize = albumResizer

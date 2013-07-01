cf7372c08f715a3162e7220a9ea04487cb0e2cbe
Srdan Kvrgic <srdan@burtcorp.com>
2013-07-02 00:28:22 +0200

    Pagination kinda works

--- galleria_app.rb
+++ galleria_app.rb
@@ -5,6 +5,15 @@ require 'sinatra'
 require 'haml'
 require 'albums'
 
+ALBUMS = 10
+
 get '/' do
+  @albums = Albums.list.take(ALBUMS)
   haml :index
+end
+
+get '/page' do
+  page = params[:page].to_i
+  @albums = Albums.list[page*ALBUMS, ALBUMS]
+  haml :page
 end
